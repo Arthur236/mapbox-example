25fd73ba99b3efb1d91554330d6e5919ef7d7297
Arthur236 <arthur.thungu@gmail.com>
2021-03-29 21:42:30 +0300

    switch to react-map-gl

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,78 +1,57 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
+import { easeCubic } from 'd3-ease';
 import * as turf from '@turf/turf';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+
+import diamond from './diamond2.png';
+import marker1 from './marker1.png';
 
 import './app.css';
-import { Button } from '@material-ui/core';
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
-const useStyles = makeStyles((theme) =>
-  createStyles({
-    placesForm: {
-      display: 'flex',
-      flexWrap: 'wrap',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      width: '400px',
-      margin: '10px auto',
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: '25ch',
-    },
-  }),
-);
-
 const App = () => {
   let map;
-  const classes = useStyles();
-
   const mapContainer = useRef();
 
-  const [values, setValues] = useState({});
-  // Initial places San Francisco, Washington DC, Toronto
-  const [placesArray, setPlaces] = useState([[-122.414, 37.776], [-77.032, 38.913], [-79.347015, 43.651070]]);
+  const destinations = [[-122.414, 37.776], [-77.032, 38.913], [-79.347015, 43.651070]];
+
+  // Used to increment the value of the point measurement against the route.
+  let counter = 0;
 
   // Number of steps to use in the arc and animation, more steps means
   // a smoother arc and animation, but too many steps will result in a
   // low frame rate
   const steps = 500;
 
-  // Used to increment the value of the point measurement against the route.
-  let counter = 0;
-
-  // A single point that animates along the route.
-  // Coordinates are initially set to origin.
-  const point = {
-    type: 'FeatureCollection',
-    features: [
+  // A simple line from origin to destination.
+  const route = {
+    'type': 'FeatureCollection',
+    'features': [
       {
-        type: 'Feature',
-        properties: {},
-        geometry: {
-          type: 'Point',
-          coordinates: placesArray[0]
+        'type': 'Feature',
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': destinations
         }
       }
     ]
   };
 
-  // A simple line from origin to destinations
-  const route = {
-    type: 'FeatureCollection',
-    features: [
+  // A single point that animates along the route.
+  // Coordinates are initially set to origin.
+  const point = {
+    'type': 'FeatureCollection',
+    'features': [
       {
-        type: "Feature",
-        geometry: {
-          type: "LineString",
-          coordinates: placesArray,
-        },
-      },
-    ],
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+          'type': 'Point',
+          'coordinates': destinations[0]
+        }
+      }
+    ]
   };
 
   useEffect(() => {
@@ -87,7 +66,7 @@ const App = () => {
     // Calculate the distance in kilometers between route start/end point.
     const lineDistance = turf.length(route.features[0]);
 
-    const arc = [];
+    let arc = [];
 
     // Draw an arc between the `origin` & `destination` of the two points
     for (let i = 0; i < lineDistance; i += lineDistance / steps) {
@@ -98,75 +77,113 @@ const App = () => {
     // Update the route with calculated arc coordinates
     route.features[0].geometry.coordinates = arc;
 
-    map.on('load', async () => {
-      await map.addSource('route', {
-        type: 'geojson',
-        data: route
+    map.on('load', () => {
+      // Add a source and layer displaying a point which will be animated in a circle.
+      map.addSource('route', {
+        'type': 'geojson',
+        'data': route
       });
 
-      await map.addSource('point', {
-        type: 'geojson',
-        data: point
+      map.addSource('point', {
+        'type': 'geojson',
+        'data': point
       });
 
-      await map.addLayer({
-        id: 'route',
-        source: 'route',
-        type: 'line',
-        paint: {
+      map.addLayer({
+        'id': 'route',
+        'source': 'route',
+        'type': 'line',
+        'paint': {
           'line-width': 2,
           'line-color': '#007cbf'
         }
       });
 
-      await map.addLayer({
-        id: 'point',
-        source: 'point',
-        type: 'symbol',
-        layout: {
-          'icon-image': 'airport-11',
-          'icon-size': 2,
-          'icon-rotate': ['get', 'bearing'],
-          'icon-rotation-alignment': 'map',
-          'icon-allow-overlap': true,
-          'icon-ignore-placement': true
-        }
+      map.loadImage(diamond, function(error, image) {
+        if (error) throw error;
+
+        map.addImage('diamond', image);
+
+        map.addLayer({
+          'id': 'point',
+          'source': 'point',
+          'type': 'symbol',
+          'layout': {
+            'icon-image': 'diamond',
+            'icon-size': 0.2,
+            'icon-rotate': ['get', 'bearing'],
+            'icon-rotation-alignment': 'map',
+            'icon-allow-overlap': true,
+            'icon-ignore-placement': true
+          }
+        });
       });
 
-      // create destination markers
-      placesArray.forEach((dest) => {
-        new mapboxgl.Marker()
-          .setLngLat(dest)
-          .setPopup(new mapboxgl.Popup({
-            offset: 25,
-            closeButton: false
-          }).setHTML('<h3>Example Popup</h3><p>Add content here</p>'))
+      // add markers to map
+      destinations.forEach(function (marker) {
+      // create a DOM element for the marker
+        let el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage = `url(${marker1})`;
+        el.style.width = '30px';
+        el.style.height = '30px';
+        el.style.backgroundSize = '100%';
+
+        el.addEventListener('click', function () {
+          map.flyTo({
+            center: marker,
+            zoom: 9,
+            bearing: 0,
+            speed: 2,
+            curve: 1,
+            easing: easeCubic,
+            essential: true
+          });
+        });
+
+        map.on('mouseenter', marker, function () {
+          map.getCanvas().style.cursor = 'pointer';
+        });
+
+        // add marker to map
+        new mapboxgl.Marker(el, { anchor: 'bottom' })
+          .setLngLat(marker)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                '<h3>Title</h3>' +
+                '<p>' + marker + '</p>'
+              )
+              .on('close', () => {
+                map.flyTo({
+                  center: [-96, 37.8],
+                  zoom: 3,
+                  bearing: 0,
+                  speed: 2,
+                  curve: 1,
+                  easing: easeCubic,
+                  essential: true
+                });
+              })
+          )
           .addTo(map);
       });
 
-      await animate(counter);
+      animate();
     });
 
     return () => map.remove();
-  }, [placesArray]);
+  }, []);
 
   const animate = () => {
-    const start =
-      route.features[0].geometry.coordinates[
-        counter >= steps ? counter - 1 : counter
-        ];
-
-    const end =
-      route.features[0].geometry.coordinates[
-        counter >= steps ? counter : counter + 1
-        ];
+    const start = route.features[0].geometry.coordinates[counter >= steps ? counter - 1 : counter];
+    const end = route.features[0].geometry.coordinates[counter >= steps ? counter : counter + 1];
 
     if (!start || !end) return;
 
     // Update point geometry to a new position based on counter denoting
     // the index to access the arc
-    point.features[0].geometry.coordinates =
-      route.features[0].geometry.coordinates[counter];
+    point.features[0].geometry.coordinates = route.features[0].geometry.coordinates[counter];
 
     // Calculate the bearing to ensure the icon is rotated to match the route arc
     // The bearing is calculated between the current point and the next point, except
@@ -187,9 +204,19 @@ const App = () => {
     counter = counter + 1;
   };
 
-  const handleClick = () => {
+  const handleRestart = () => {
+    map.flyTo({
+      center: [-96, 37.8],
+      zoom: 3,
+      bearing: 0,
+      speed: 2,
+      curve: 1,
+      easing: easeCubic,
+      essential: true
+    });
+
     // Set the coordinates of the original point back to origin
-    point.features[0].geometry.coordinates = placesArray[0];
+    point.features[0].geometry.coordinates = origin;
 
     // Update the source layer
     map.getSource('point').setData(point);
@@ -198,63 +225,19 @@ const App = () => {
     counter = 0;
 
     // Restart the animation
-    animate(counter);
-  };
-
-  const handleChange = (e) => {
-    setValues({
-      ...values,
-      [e.target.name]: e.target.value
-    })
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setPlaces([...placesArray, [values.lng, values.lat]]);
+    animate();
   };
 
   return (
     <div className="map-wrapper">
       <h2>Mapbox Test</h2>
 
-      <div className="map-container">
-        <button className="replay-btn" onClick={handleClick}>
-          Replay
-        </button>
+      <div className="map-container" ref={mapContainer}>
+        <div id="map"/>
 
-        <div className="map" ref={mapContainer}/>
-      </div>
-
-      <div className={classes.placesForm}>
-        <h2>Add a place</h2>
-
-        <form onSubmit={handleSubmit}>
-          <TextField
-            label="Lat"
-            name="lat"
-            placeholder="Latitude"
-            fullWidth
-            margin="normal"
-            type="float"
-            onChange={handleChange}
-          />
-
-          <TextField
-            label="Lng"
-            name="lng"
-            placeholder="Longitude"
-            fullWidth
-            margin="normal"
-            type="float"
-            helperText="Use a negative value eg: -38.765"
-            onChange={handleChange}
-          />
-
-          <Button type="submit" variant="contained" color="primary" fullWidth>
-            Add Place
-          </Button>
-        </form>
+        <div className="overlay">
+          <button id="replay-btn" onClick={handleRestart}>Replay</button>
+        </div>
       </div>
     </div>
   );
